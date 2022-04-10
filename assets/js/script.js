9448302a1072f572c10cb86cf7ad7178188df999
Dean <deank242@gmail.com>
2022-04-10 14:26:00 -0400

    final updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,74 +6,87 @@ var miamiBtn = document.getElementById("miami");
 var tallahasseeBtn = document.getElementById("tallahassee");
 var tampaBtn = document.getElementById("tampa");
 var restList = document.querySelector(".rest-list");
-var currentPicture = document.getElementById("picture")
-var currentTemperature = document.getElementById("temperature")
-var currentHumidity = document.getElementById("humidity")
-var currentWind = document.getElementById("wind")
-var currentUv = document.getElementById("uv")
-
+var mapWeather = document.querySelector(".map-weather");
+var listEl = document.querySelector(".list");
+var weatherBox = document.querySelector(".weather-box");
 
 
 // Submit btn for each cuisine
 jacksonvilleBtn.addEventListener("click", function() {
+    document.getElementById("input-box").value = "";
+    document.getElementById("input-box").value = "Jacksonville";
     getJaxFood();
     restList.classList.remove("hide");
+    mapWeather.classList.remove('hide');
+    weatherBox.classList.remove("hide");
+    listEl.classList.remove("hide");
     document.querySelector(".map").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d440729.51651678846!2d-81.9632966213798!3d30.345283956447123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e5b716f1ceafeb%3A0xc4cd7d3896fcc7e2!2sJacksonville%2C%20FL!5e0!3m2!1sen!2sus!4v1649385488166!5m2!1sen!2sus" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
-    weather();
+    
     
 })
 
 orlandoBtn.addEventListener("click", function() {
+    document.getElementById("input-box").value = "";
+    document.getElementById("input-box").value = "Orlando";
     getOrlandoFood();
     restList.classList.remove("hide");
+    mapWeather.classList.remove('hide');
+    weatherBox.classList.remove("hide");
+    listEl.classList.remove("hide");
     document.querySelector(".map").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d156405.32205820302!2d-81.43697875387006!3d28.503071492868465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e773d8fecdbc77%3A0xac3b2063ca5bf9e!2sOrlando%2C%20FL!5e0!3m2!1sen!2sus!4v1649385232651!5m2!1sen!2sus" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 })
 
 miamiBtn.addEventListener("click", function() {
+    document.getElementById("input-box").value = "";
+    document.getElementById("input-box").value = "Miami";
     getMiamiFood();
     restList.classList.remove("hide");
+    mapWeather.classList.remove('hide');
+    weatherBox.classList.remove("hide");
+    listEl.classList.remove("hide");
     document.querySelector(".map").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114964.38943768624!2d-80.29949876361637!3d25.7825453114265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b0a20ec8c111%3A0xff96f271ddad4f65!2sMiami%2C%20FL!5e0!3m2!1sen!2sus!4v1649385602465!5m2!1sen!2sus" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
-    
 })
 
 tallahasseeBtn.addEventListener("click", function() {
+    document.getElementById("input-box").value = "";
+    document.getElementById("input-box").value = "Tallahassee";
     getTallahasseeFood();
     restList.classList.remove("hide");
+    mapWeather.classList.remove('hide');
+    weatherBox.classList.remove("hide");
+    listEl.classList.remove("hide");
     document.querySelector(".map").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110044.69809812971!2d-84.32690328513473!3d30.46736059428571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88ec8a5187124b53%3A0xebee077ad4fdb1f8!2sTallahassee%2C%20FL!5e0!3m2!1sen!2sus!4v1649385697960!5m2!1sen!2sus" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 })
 
 tampaBtn.addEventListener("click", function() {
+    document.getElementById("input-box").value = "";
+    document.getElementById("input-box").value = "Tampa";
     getTampaFood();
     restList.classList.remove("hide");
+    mapWeather.classList.remove('hide');
+    weatherBox.classList.remove("hide");
+    listEl.classList.remove("hide");
     document.querySelector(".map").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d225470.28811533237!2d-82.59436588351026!3d27.99471474428446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2b782b3b9d1e1%3A0xa75f1389af96b463!2sTampa%2C%20FL!5e0!3m2!1sen!2sus!4v1649385742328!5m2!1sen!2sus" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 })
 
 var getJaxFood = function() {
     Cities(30.3322, -81.6557);
-    weather(30.3322, -81.6557);
 }
     
 var getTampaFood = function() {
     Cities(27.9506, -82.4572);
-    weather(27.9506, -82.4572)
 }
 
 var getOrlandoFood = function() {
     Cities(28.5384, -81.3789);
-    weather(28.5384, -81.3789)
-
 }
 
 var getMiamiFood = function() {
     Cities(25.7617, -80.1918);
-    weather(25.7617, -80.1918)
-
 }
 
 var getTallahasseeFood = function() {
     Cities(30.4383, -84.2807);
-    weather(30.4383, -84.2807)
-
 }
 
 var Cities = function(latitude, longitude) {
@@ -110,58 +123,33 @@ var Cities = function(latitude, longitude) {
                             };
                 });
 
-                
+                var apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&units=imperial&exclude=minutely,hourly,alerts&appid=7f371e4052b1c98d236355c1a3d79224'
+                ;
+
+                    //make a request to the url for the lat/lon
+                    fetch(apiUrl2)
+                    .then(response => response.json())
+                    .then(response => {
+                        console.log(response.current); 
+                            
+                        var weatherBox = document.querySelector(".weather-box");
+
+                            const {temp, wind_speed, humidity, uvi} = response.current;
+                            const {icon} = response.current.weather[0];
+
+                            console.log(temp, wind_speed, humidity, uvi, icon);
+
+                                weatherBox.innerHTML = `<div class="w-box"><b>Today's Weather:</b>
+                                                        <div class="icon-img"><img src="https://openweathermap.org/img/w/${icon}.png"></div>
+                                                        <div>Temp:&nbsp ${temp}°F</div>
+                                                        <div>Humidity:&nbsp ${humidity}%</div>
+                                                        <div>Wind:&nbsp ${wind_speed}%</div>
+                                                        <div>UVI:&nbsp ${uvi}%</div>
+                                                        </div>`;
+                                
+                            }) 
                    
             })
             .catch(err => console.error(err));
-
-    
-    
-            
-}
-
-var weather = function(latitude, longitude) {
-    var apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude + '&lon=' +longitude + '&units=imperial&exclude=minutely,hourly,alerts&appid=4253bc5e2147b614fe1510a5ddf5c0c5';
-
-    //make a request to the url for the lat/lon
-    fetch(apiUrl2)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response.current.temp)
-    var dataResponse = response.current;
-        var weather = document.querySelector(".weather");
-
-        const {temp, wind_speed, humidity, uvi} = response.current;
-        const {icon} = response.current.weather[0];
-
-                
-                let weatherImage = dataResponse.current.weather[0].icon;
-                currentPicture.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherImage + "@2x.png");
-                
-    
-
-                const forecastTempEl = document.createElement("p");
-                currentTemperature.innerHTML = "Temperature: " + k2f(dataResponse.temp) + "&#176F";
-                
-
-
-                // var weatherData = {
-                //     temp:  data.daily[i].temp.day,
-                //     humidity: data.daily[i].humidity,
-                //     icon: data.current.weather[0].icon
-                // };
-
-                console.log(weatherData);
-
-                document.querySelector(".weather").innerHTML = `<div class="weatherInfo">
-                <div class="w-icon"><img src="https://openweathermap.org/img/w/${weatherData.icon}.png"></div>
-                <div class="weatherInfo">
-                <div>Temp: ${weatherData.temp}°F</div>
-                <div>Humidity: ${weatherData.humidity}%</div>
-                <div>Wind speed: ${weathe}
-                </div>`;
-                
-            }) 
-        
-            console.log(currentTemperature)
+      
 }
